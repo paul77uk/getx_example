43d127a67ad6506d02e745ca464c112cf9b384e2
paul77uk <paul77uk@gmail.com>
2020-08-06 11:25:15 +0100

    Update controller.dart

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,12 +1,15 @@
 import 'package:get/get.dart';
 
 class Controller extends GetxController {
-  var count = 0.obs;
-//   var name = 'Jonatas Borges'.obs;
+   var count = 0.obs;
+  var count2 = 0.obs;
+  var total = 0.obs;
+  // var name = 'Jonatas Borges'.obs;
   
   increment() => count.value++;
-  
+  increment2() => count2.value++;
   decrement() => count.value--;
+  sum() => total.value = count.value + count2.value;
   
 //   plusTen() => count.value = 10;
   
