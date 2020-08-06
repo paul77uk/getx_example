1b6ce90374c1bb13389a7c1ae405f5ef1aa4c7af
paul77uk <paul77uk@gmail.com>
2020-08-06 10:35:22 +0100

    Update controller.dart

--- lib/controller.dart
+++ lib/controller.dart
@@ -3,18 +3,22 @@ import 'package:get/get.dart';
 class Controller extends GetxController {
   var count = 0.obs;
   var name = 'Jonatas Borges'.obs;
+  
   increment() => count.value++;
+  
   decrement() => count.value--;
+  
   plusTen() => count.value = 10;
+  
   changeValue(value) {
     count.value = value;
   }
 
-  static Controller get to => Get.find();
+//   static Controller get to => Get.find();
 
-  int counter = 0;
-  void incrementCounter() {
-    counter++;
-    update(); // use update() to update counter variable on UI when increment be called
-  }
+//   int counter = 0;
+//   void incrementCounter() {
+//     counter++;
+//     update(); // use update() to update counter variable on UI when increment be called
+//   }
 }
