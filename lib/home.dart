7e4824291506dfbae6500c0b285f11cd6f3cdef4
paul77uk <paul77uk@gmail.com>
2020-08-06 11:41:59 +0100

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -24,20 +24,7 @@ class Home extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // On your Stateless/Stateful class, use GetBuilder to update Text when increment be called
-            // GetBuilder<Controller>(
-            //   init: Controller(), // INIT IT ONLY THE FIRST TIME
-            //   builder: (_) => Text(
-            //     '${_.counter}',
-            //   ),
-            // ),
-//Initialize your controller only the first time. The second time you are using ReBuilder for the same controller, do not use it again. Your controller will be automatically removed from memory as soon as the widget that marked it as 'init' is deployed. You don't have to worry about that, Get will do it automatically, just make sure you don't start the same controller twice.
-            // Text("${Controller.to.counter}"),
-
-            // RaisedButton(
-            //   child: Text("Increment counter"),
-            //   onPressed: () => Controller.to.increment(),
-            // ),
+           
             Obx(() => Text('count2: ${c.count2.value}')),
             Obx(() => Text('sum = ${c.total.value}')),
             RaisedButton(
